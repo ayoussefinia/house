6b44bed1dbda106c5c120499be759fdcece313ce
ayoussefinia <alex.youssefinia@gmail.com>
2020-08-17 19:22:04 -0500

    created user mongoose model and graphql mutation which saves a hashed password to the database and returns a signed JWT

--- server/index.js
+++ server/index.js
@@ -3,17 +3,22 @@ import bodyParser from 'body-parser';
 import expressGraphQL from 'express-graphql';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import graphQLSchema from './graphql/schema';
+import graphQLResolvers from './graphql/resolvers';
 require('dotenv').config();
+
 const app = express();
+
 app.use(
   cors(),
   bodyParser.json()
 )
+
 app.use(
   "/graphql",
   expressGraphQL({
-  schema: {},
-  rootValue: {},
+  schema: graphQLSchema,
+  rootValue: graphQLResolvers,
   graphiql: true
   })
 );
